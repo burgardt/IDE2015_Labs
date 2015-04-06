b777c03671c314d78df6484fde03de0a59a400f9
Mauro Gimenez <mauro88rc@gmail.com>
2015-04-06 21:41:37 +0100

    3er commit

---
+++ Unidad2_lab02/Unidad2_lab02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidad2_lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
